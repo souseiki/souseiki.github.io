3cdf3610cc4c094ced3d1d40bf0f40e770dd3fed
hanxin <hanxin1@geely.com>
2020-03-20 09:20:24 +0800

    Site updated: 2020-03-20 09:20:24

--- libs/background/rain-code.js
+++ libs/background/rain-code.js
@@ -1,113 +1,46 @@
 ! function () {
-    var userAgentInfo = navigator.userAgent;
-    var Agents = ["iPad", "iPhone", "Android",
-        "SymbianOS", "Windows Phone",
-        "iPod", "webOS", "BlackBerry", "IEMobile"
-    ];
-    for (var v = 0; v < Agents.length; v++) {
-        if (userAgentInfo.indexOf(Agents[v]) > 0) {
-            return;
+    //获取屏幕可视区域大小
+    var d = document.documentElement;
+    var clinetW = d.clientWidth;
+    var clinetH = d.clientHeight;
+
+    //设置画布大小
+    var canvas = document.querySelector('canvas');
+    canvas.width = clinetW;
+    canvas.height = clinetH;
+
+    var cxt = canvas.getContext('2d');
+    var rainStr = 'The matrix of hackers';
+    var arr = rainStr.split('');
+
+    var fontSize = 14;
+    // 计算行数
+    var cols = Math.floor(clinetW / fontSize);
+    // 初始化Y轴坐标
+    var down = [];
+    for (var i = 0; i < cols; i++) {
+    down.push(Math.floor(Math.random() * -100));
+    }
+
+    function drawRain() {
+    // 填充背景(ps:背景采用rgba,可尝试不同透明度的效果)
+    cxt.fillStyle = 'rgba(0,0,0,.1)';
+    cxt.fillRect(0, 0, clinetW, clinetH);
+
+    // 设置字体颜色
+    cxt.fillStyle = '#00ff00';
+    for (var i = 0; i < down.length; i++) {
+        var randomNum = Math.random();
+        // 绘制文字
+        cxt.fillText(arr[Math.floor(randomNum * arr.length)], i * fontSize, down[i] * fontSize);
+
+        if (down[i] * fontSize > clinetH && randomNum > 0.9) {
+        down[i] = 0;
         }
+        //绘制文字的下一个位置
+        down[i]++;
+    }
     }
 
-    function o(w, v, i) {
-        return w.getAttribute(v) || i
-    }
-
-    function j(i) {
-        return document.getElementsByTagName(i)
-    }
-
-    function l() {
-        var i = j("script"),
-            w = i.length,
-            v = i[w - 1];
-        return {
-            l: w,
-            z: o(v, "zIndex", -1),
-            o: o(v, "opacity", 0.5),
-            c: o(v, "color", "0,0,0"),
-            n: o(v, "count", 99)
-        }
-    }
-
-    function k() {
-        r = u.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth, n = u.height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
-    }
-
-    function b() {
-        e.clearRect(0, 0, r, n);
-        var w = [f].concat(t);
-        var x, v, A, B, z, y;
-        t.forEach(function (i) {
-            i.x += i.xa, i.y += i.ya, i.xa *= i.x > r || i.x < 0 ? -1 : 1, i.ya *= i.y > n || i.y < 0 ? -1 : 1, e.fillRect(i.x - 0.5, i.y - 0.5, 1, 1);
-            for (v = 0; v < w.length; v++) {
-                x = w[v];
-                if (i !== x && null !== x.x && null !== x.y) {
-                    B = i.x - x.x, z = i.y - x.y, y = B * B + z * z;
-                    y < x.max && (x === f && y >= x.max / 2 && (i.x -= 0.03 * B, i.y -= 0.03 * z), A = (x.max - y) / x.max, e.beginPath(), e.lineWidth = A / 2, e.strokeStyle = "rgba(" + s.c + "," + (A + 0.2) + ")", e.moveTo(i.x, i.y), e.lineTo(x.x, x.y), e.stroke())
-                }
-            }
-            w.splice(w.indexOf(i), 1)
-        }), m(b)
-    }
-    var u = document.createElement("canvas"),
-        s = l(),
-        c = "c_n" + s.l,
-        e = u.getContext("2d"),
-        r, n, m = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (i) {
-            window.setTimeout(i, 1000 / 45)
-        },
-        a = Math.random,
-        f = {
-            x: null,
-            y: null,
-            max: 20000
-        };
-    u.id = c;
-    u.style.cssText = "position:fixed;top:0;left:0;z-index:" + s.z + ";opacity:" + s.o;
-    j("body")[0].appendChild(u);
-    k(), window.onresize = k;
-    ar fontSize = 12;
-    //计算列
-    var colunms = Math.floor(W /fontSize);	
-    //记录每列文字的y轴坐标
-    var drops = [];
-    //给每一个文字初始化一个起始点的位置
-    for(var z=0;z<colunms;z++){
-        drops.push(0);
-    }
-
-    //运动的文字
-    var str ="WELCOME TO WWW.ITRHX.COM";
-    //4:fillText(str,x,y);原理就是去更改y的坐标位置
-    //绘画的函数
-    function draw(){
-        e.fillStyle = "rgba(238,238,238,.08)";//遮盖层
-        e.fillRect(0,0,W,H);
-        //给字体设置样式
-        e.font = "600 "+fontSize+"px  Georgia";
-        //给字体添加颜色
-        e.fillStyle = ["#33B5E5", "#0099CC", "#AA66CC", "#9933CC", "#99CC00", "#669900", "#FFBB33", "#FF8800", "#FF4444", "#CC0000"][parseInt(Math.random() * 10)];//randColor();可以rgb,hsl, 标准色，十六进制颜色
-        //写入画布中
-        for(var i=0;i<colunms;i++){
-            var index = Math.floor(Math.random() * str.length);
-            var x = i*fontSize;
-            var y = drops[i] *fontSize;
-            e.fillText(str[index],x,y);
-            //如果要改变时间，肯定就是改变每次他的起点
-            if(y >= canvas.height && Math.random() > 0.99){
-                drops[i] = 0;
-            }
-            drops[i]++;
-        }
-    };
-    function randColor(){//随机颜色
-        var r = Math.floor(Math.random() * 256);
-        var g = Math.floor(Math.random() * 256);
-        var b = Math.floor(Math.random() * 256);
-        return "rgb("+r+","+g+","+b+")";
-    }
-    draw();
-    setInterval(draw,35);
+    setInterval(drawRain, 30);
 }();
